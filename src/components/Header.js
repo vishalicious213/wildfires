9f3ded6e32b72e0dadc79f1a89931fd3185b9244
Vish Singh <vishalicious213@gmail.com>
2020-11-20 14:06:53 -0800

    added & styled Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import locationIcon from '@iconify/icons-mdi/fire-alert'
 const Header = () => {
     return (
         <header className='header'>
-            <h1><Icon icon={locationIcon} />Wildfire Tracker (Powered by NASA)</h1>
+            <h1><Icon icon={locationIcon} /> Wildfire Tracker (Powered by NASA)</h1>
         </header>
     )
 }
