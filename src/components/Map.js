35c1fe1faba873d06502efc5583f96680f6dac28
Vish Singh <vishalicious213@gmail.com>
2020-11-20 11:26:36 -0800

    set up Google Maps Platform, got API key, set up dotenv and .env file with key. drafted Map.js

---
+++ src/components/Map.js
@@ -0,0 +1,24 @@
+import GoogleMapReact from 'google-map-react'
+
+// Takes in centerpoint of map and zoom level
+const Map = ({ center, zoom}) => {
+    return (
+        <div className='map'>
+            <GoogleMapReact>
+                bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
+                defaultCenter={ center }
+                defaultZoom={ zoom }
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 42.3265,   // default latitude
+        lng: -122.8756  // default longitude
+    },
+    zoom: 6
+}
+
+export default Map
