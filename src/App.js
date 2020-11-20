4317d6f41a79bd2a1cb5d12477438b33fd791cca
Vish Singh <vishalicious213@gmail.com>
2020-11-20 12:11:07 -0800

    fetch events data from NASA API in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 // Note: We're not importing React anymore!
+import { useState, useEffect } from 'react'
 import Map from './components/Map'
 
 function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const response = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await response.json()  // Get & destructure the events array
+
+      setEventData(events)
+      setLoading(false)
+    }
+  })
+
   return (
     <div>
       <Map />
