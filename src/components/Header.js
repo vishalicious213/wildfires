1dfeb2ab8bf58ad602ecd062c28ee9cde3fb3ab1
Vish Singh <vishalicious213@gmail.com>
2020-11-20 15:37:15 -0800

    styled infobox, header, wildfire icons & loader

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import locationIcon from '@iconify/icons-mdi/fire-alert'
 const Header = () => {
     return (
         <header className='header'>
-            <h1><Icon icon={locationIcon} /> Wildfire Tracker (Powered by NASA)</h1>
+            <Icon icon={locationIcon} className='icon' />
+            <h1>Wildfire Tracker</h1>
+            <Icon icon={locationIcon} className='icon' />
         </header>
     )
 }
