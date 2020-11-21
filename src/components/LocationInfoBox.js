f5616a203cccedfb159a8079d0b47ea74773e44b
Vish Singh <vishalicious213@gmail.com>
2020-11-21 15:51:51 -0800

    formatted date output with substring. changed style of header & location info. changed order of data in location info

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -1,9 +1,9 @@
 const LocationInfoBox = ({ info }) => {
     return (
         <div className='location-info'>
-            <p><strong>Title: </strong>{ info.title }</p>
-            <p><strong>ID: </strong>{ info.id }</p>
             <p><strong>Date: </strong>{ info.date }</p>
+            <p><strong>ID: </strong>{ info.id }</p>
+            <p><strong>Title: </strong>{ info.title }</p>
         </div>
     )
 }
